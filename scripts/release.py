dfc65c048d68627dac8ccef289d04e2ca1b2d113
Alin Voinea <contact@avoinea.com>
2020-10-13 19:56:50 +0300

    Fix release script

--- scripts/release.py
+++ scripts/release.py
@@ -5,24 +5,31 @@ import json
 from pprint import pprint
 import subprocess
 import argparse
+import urllib.request
+
+URL = "https://raw.githubusercontent.com/eea/ims-frontend/master/package.json"
 
 def main(verbose=True):
-    RELEASES = {}
-    TO_BE_RELEASED = []
+    versions = {}
+    to_be_release = []
 
-    with open("package.json", 'r') as ofile:
+    with urllib.request.urlopen(URL) as ofile:
         config = json.loads(ofile.read())
 
     for package, version in config['dependencies'].items():
         tag = version.split("#")[-1]
-        RELEASES[package] = tag
+        versions[package] = tag
 
     with open("jsconfig.json", "r") as ofile:
         config = json.loads(ofile.read())
 
     for addon, paths in config['compilerOptions']['paths'].items():
         path = paths[0]
-        release = RELEASES[addon]
+        release = versions.get(addon)
+        if not release:
+            to_be_release.append(addon)
+            continue
+
         with subprocess.Popen(
             ["git", "log", "--pretty=oneline", "--abbrev-commit", "%s..HEAD" % release],
             cwd=os.path.join(os.getcwd(), "src", path), stdout=subprocess.PIPE) as proc:
@@ -31,8 +38,8 @@ def main(verbose=True):
                 if(verbose):
                     print("==================== %s " % path)
                     print(res.decode('utf-8'))
-                TO_BE_RELEASED.append(addon)
-    return TO_BE_RELEASED
+                to_be_release.append(addon)
+    return to_be_release
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
