cff846dda874f6221135f545c90782a61da3545b
Alin Voinea <contact@avoinea.com>
2021-02-25 13:29:22 +0200

    Upgrade to Volto 12

--- patches/patchit.sh
+++ patches/patchit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "No patches"
+patch --quiet -p0 -N node_modules/razzle/config/createJestConfig.js < patches/razzle-jest.patch
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@
 
 import { App } from '@plone/volto/components';
 import { defaultRoutes } from '@plone/volto/routes';
+import config from '@plone/volto/registry';
 
 /**
  * Routes array.
@@ -17,6 +18,7 @@ const routes = [
     component: App, // Change this if you want a different component
     routes: [
       // Add your routes here
+      ...(config.addonRoutes || []),
       ...defaultRoutes,
     ],
   },
